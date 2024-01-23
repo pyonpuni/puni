a5dc00b9976548f2f416f20179e5e0f061dd74a6
pyonpuni <157256898+pyonpuni@users.noreply.github.com>
2024-01-23 09:12:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ client.on('ready', async () => {
     .setApplicationId('1170948236769493052')
     .setType('STREAMING')
     .setURL('https://twitch.tv/developer') //Must be a youtube video link 
-    .setState('お願い！ﾀﾋんでくれ')
+    .setState('永遠愛在一起')
     .setName('maso')
-    .setDetails(`།𓎟𓎟𓎟†𓎟𓎟𓎟།`)
+    .setDetails(`།𓎟𓎟†𓎟𓎟།`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1011444419838353429/1198449882822885467/33C636B2-9B0C-4B41-BB68-8C4DF0F6B7A9_8.gif?ex=65bef26e&is=65ac7d6e&hm=1c56b2eaee6dc1e987267f95e5de06549857c58c89e5372e12fbe059d64804f9&') //You can put links in tenor or discord and etc.
-.setAssetsSmallImage('https://cdn.discordapp.com/attachments/1011444419838353429/1198449882428625017/IMG_5774.gif?ex=65bef26e&is=65ac7d6e&hm=5f2ab6e0ea470cc7beb597f5363717c581c8ef43279d832356349f9e31e890b7&')
-    .setAssetsLargeText('なりたい　なれない') //Text when you hover the Large image
-    .addButton('♡', 'https://rentry.co/')
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1011444419838353429/1199368605565992980/Untitled574_20240123085905.png?ex=65c24a0e&is=65afd50e&hm=e3797548a81577836a18193dd0fc8f6bcf5a1b5b524666fa4c7b694b1e404f82&') //You can put links in tenor or discord and etc.
+.setAssetsSmallImage('https://media.discordapp.net/attachments/1011444419838353429/1199368606010572980/Untitled550_20240123085831.png?ex=65c24a0e&is=65afd50e&hm=4671c01048c29c88c30a7164c72b467101ab909391953474ca817a60407e2684&')
+    .setAssetsLargeText('在一起真好') //Text when you hover the Large image
+    .addButton('♡', 'https://rentry.co/pien')
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -42,7 +42,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `།𓎟𓎟𓎟†𓎟𓎟𓎟།`;
+      const newDetails = `།𓎟𓎟†𓎟𓎟།`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
