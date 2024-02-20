c127a25436429dfc51417cf8552552dd92cedf4e
pyonpuni <157256898+pyonpuni@users.noreply.github.com>
2024-02-20 12:13:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ client.on('ready', async () => {
     .setApplicationId('1208170235589431317')
     .setType('STREAMING')
     .setURL('https://twitch.tv/developer') //Must be a youtube video link 
-    .setState('𝗒𝗈𝗎 𝗹𝗲𝘁 𝗺𝗲 𝖼𝗋𝗒...')
+    .setState('𝖺𝗅𝗅𝗈𝗐 𝗺𝗲 𝗍𝗈 𝗆𝖺𝗄𝖾 𝘆𝗼𝘂 𝖺𝗇')
     .setName('maso')
     .setDetails(`𝗶'𝗹𝗹 𝘁𝗮𝗸𝗲 𝗶𝘁 𝗮𝗹𝗹 .ᐟ`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1011444419838353429/1209562592343883866/Untitled644_20240220111029_1.gif?ex=65e75ff2&is=65d4eaf2&hm=30706a61e23690338726025a0fca2eca5935474153daca35b19886e9356fd5e3&') //You can put links in tenor or discord and etc.
 .setAssetsSmallImage('https://media.discordapp.net/attachments/1011444419838353429/1209562835554795621/IMG_7539.png?ex=65e7602c&is=65d4eb2c&hm=1f08e61c53bbe9e6e888a6442f1adb5863b3d811b4d5a251926a0bc021f4323a&')
-    .setAssetsLargeText('𝖺𝗇𝖽 𝘄𝗶𝗽𝗲 𝗺𝘆 𝖾𝗒𝖾𝗌') //Text when you hover the Large image
+    .setAssetsLargeText('𝗼𝗳𝗳𝗲𝗿. 𝗈𝗇𝖾 𝗒𝗈𝗎 𝗰𝗮𝗻𝘁 𝗋𝖾𝖿𝗎𝗌𝖾') //Text when you hover the Large image
     .addButton('⟡', 'https://rentry.co/avginshajin')
 
   client.user.setActivity(r);
